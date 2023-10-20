0be25d6c6095c697b1dbbff96a51453e0e89b73d
Nilanshuyadav <yadavnilanshu7011@gmail.com>
2023-10-20 14:35:08 +0530

    initial commit

---
+++ Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+// interface A{
+//     public abstract void F();
+// }
+
+// // class B : A{
+// //     public  sealed void F(){
+// //         Console.WriteLine("in class B");
+// //     }
+// // }
+
+class C {
+
+    public static void Main(string[] args)
+    {
+        string s = "hello";
+
+        Console.WriteLine(s);
+    }
+}
---
+++ tes.cs
@@ -0,0 +1,25 @@
+// using System;
+// using System.Collections.Generic;
+// using System.Diagnostics.CodeAnalysis;
+// using System.Linq;
+// using System.Threading.Tasks;
+
+// namespace _testing_
+// {
+
+//     public class blah{
+//         public virtual int sum(){
+//             return 10;
+//         }
+//     }
+//     public class tes: blah
+//     {
+//         // public override int sum(){
+//         //     return 5;
+//         // }
+
+//         public static void Main(){
+//             Console.WriteLine(new tes().sum());
+//         }
+//     }
+// }
